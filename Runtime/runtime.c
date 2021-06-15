20f3203b221c07959c6f06b667bb52d9981b9017
Dimitri Racordon <dimitri.racordon@gmail.com>
2021-06-16 01:03:03 +0200

    Optimize the runtime representation of arrays

--- Runtime/runtime.c
+++ Runtime/runtime.c
@@ -33,19 +33,18 @@ typedef struct {
 /// A type-erased array.
 typedef struct {
 
-  /// The number of elements in the array.
-  int64_t count;
-
-  /// The capacity of the array's storage, in bytes.
-  int64_t capacity;
-
-  /// A pointer to the array's reference counter.
-  int64_t* refcount;
-
   /// A pointer to the array's storage.
+  ///
+  /// The storage has the following layout:
+  ///
+  ///     { refcount: i64; count: i64; capacity: i64; elements: T[capacity] }
+  ///
+  /// If the pointer is null, then it is assumed that the array has a 0 capacity.
   void* storage;
 
 } mvs_AnyArray;
+
+#define ARRAY_HEADER ((int64_t)sizeof(int64_t) * 3)
 
 void* mvs_malloc(int64_t size) {
   void* buf = malloc(size);
@@ -64,25 +63,31 @@ void mvs_array_init(mvs_AnyArray* array,
 #ifdef DEBUG
   fprintf(stderr, "mvs_array_init(%p, %p, %lli, %lli)\n", array, elem_type, count, size);
 #endif
-  array->count = count;
-  array->refcount = malloc(sizeof(int64_t));
-  *(array->refcount) = 1;
 
   if (count > 0) {
-    array->capacity = count * size;
-    array->storage = malloc(array->capacity);
+    // Allocate new storage.
+    int64_t capacity = count * size;
+    array->storage = malloc(ARRAY_HEADER + capacity);
 #ifdef DEBUG
-    fprintf(stderr, "  alloc %lli bytes at %p\n", count * size, array->storage);
+    fprintf(stderr, "  alloc %lli+%lli bytes at %p\n", ARRAY_HEADER, capacity, array->storage);
 #endif
+
+    // Configure the storage's header.
+    int64_t* header = (int64_t*)array->storage;
+    header[0] = 1;
+    header[1] = count;
+    header[2] = capacity;
+
+    // Initialize the storage's payload.
+    void* payload = array->storage + ARRAY_HEADER;
     if (elem_type->init != NULL) {
       for (size_t i = 0; i < count; ++i) {
-        elem_type->init(&array->storage[i * size]);
+        elem_type->init(&payload[i * size]);
       }
     } else {
-      memset(array->storage, 0, count * size);
+      memset(payload, 0, count * size);
     }
   } else {
-    array->capacity = 0;
     array->storage = NULL;
   }
 }
@@ -97,17 +102,19 @@ void mvs_array_drop(mvs_AnyArray* array, const mvs_MetaType* elem_type) {
   fprintf(stderr, "mvs_array_drop(%p, %p)\n", array, elem_type);
 #endif
 
-  if (*(array->refcount) > 1) {
-    *(array->refcount) -= 1;
+  int64_t* header = (int64_t*)array->storage;
+  if ((header != NULL) && (header[0] > 1)) {
+    header[0] -= 1;
 #ifdef DEBUG
-    fprintf(stderr, "  release %p (%lli)\n", array->storage, *array->refcount);
+    fprintf(stderr, "  release %p (%lli)\n", array->storage, header[0]);
 #endif
     return;
   }
 
   if (elem_type->drop != NULL) {
-    for (size_t i = 0; i < array->count; ++i) {
-      elem_type->drop(&array->storage[i * elem_type->size]);
+    void* payload = array->storage + ARRAY_HEADER;
+    for (size_t i = 0; i < header[1]; ++i) {
+      elem_type->drop(&payload[i * elem_type->size]);
     }
   }
 
@@ -115,9 +122,8 @@ void mvs_array_drop(mvs_AnyArray* array, const mvs_MetaType* elem_type) {
   fprintf(stderr, "  dealloc %p\n", array->storage);
 #endif
 
-  free(array->refcount);
   free(array->storage);
-  memset(array, 0, sizeof(mvs_AnyArray));
+  array->storage = NULL;
 }
 
 void mvs_array_copy(mvs_AnyArray* dst, mvs_AnyArray* src) {
@@ -125,12 +131,14 @@ void mvs_array_copy(mvs_AnyArray* dst, mvs_AnyArray* src) {
   fprintf(stderr, "mvs_array_copy(%p, %p)\n", dst, src);
 #endif
 
-  memcpy(dst, src, sizeof(mvs_AnyArray));
-  *(src->refcount) += 1;
+  // Trivial if the right hand side is null.
+  dst->storage = src->storage;
+  if (src->storage != NULL) {
+    *((int64_t*)src->storage) += 1;
 #ifdef DEBUG
-  fprintf(stderr, "mvs_array_copy(%p, %p)\n", dst, src);
-  fprintf(stderr, "  retain  %p (%lli)\n", src->storage, *src->refcount);
+    fprintf(stderr, "  retain  %p (%lli)\n", src->storage, *((int64_t*)src->storage));
 #endif
+  }
 }
 
 /// Creates a unique copy of `array`'s storage.
@@ -144,30 +152,32 @@ void mvs_array_uniq(mvs_AnyArray* array, const mvs_MetaType* elem_type) {
 #endif
 
   // Nothing to do if the array's already unique.
-  if (*(array->refcount) <= 1) { return; }
+  int64_t* header = (int64_t*)array->storage;
+  if ((header == NULL) || (header[0] <= 1)) { return; }
 
   // Allocate a new storage.
-  void* unique_storage = malloc(array->capacity);
+  void* unique_storage = malloc(ARRAY_HEADER + header[2]);
 #ifdef DEBUG
-    fprintf(stderr, "  alloc %lli bytes at %p\n", array->capacity, unique_storage);
+  fprintf(stderr, "  alloc %lli+%lli bytes at %p\n", ARRAY_HEADER, header[2], unique_storage);
 #endif
 
   // Copy the contents of the current storage.
   if (elem_type->copy == NULL) {
-    memcpy(unique_storage, array->storage, array->capacity);
+    memcpy(unique_storage, array->storage, ARRAY_HEADER + header[2]);
   } else {
-    for (size_t i = 0; i < array->count; ++i) {
-      elem_type->copy(&unique_storage[i * elem_type->size],
-                      &array->storage[i * elem_type->size]);
+    void* src = array->storage + ARRAY_HEADER;
+    void* dst = unique_storage + ARRAY_HEADER;
+    for (size_t i = 0; i < header[1]; ++i) {
+      elem_type->copy(&dst[i * elem_type->size], &src[i * elem_type->size]);
     }
   }
 
   // Decrement the reference counter on the old storage.
-  *(array->refcount) -= 1;
+  header[0] -= 1;
 
   // Substitute the old array's storage.
-  array->refcount = malloc(sizeof(int64_t));
-  *(array->refcount) = 1;
+  int64_t* new_header = (int64_t*)unique_storage;
+  new_header[0] = 1;
   array->storage = unique_storage;
 }
 
@@ -185,18 +195,22 @@ int64_t mvs_array_equal(const mvs_AnyArray* lhs,
   if (lhs->storage == rhs->storage) { return 1; }
 
   // Check for element-wise equality.
-  if (lhs->count == rhs->count) {
-    for (int64_t i = 0; i < lhs->count; ++i) {
-      void* a = &lhs->storage[i * elem_type->size];
-      void* b = &rhs->storage[i * elem_type->size];
-      if (elem_type->equal(a, b) == 0) {
-        return 0;
-      }
-    }
-    return 1;
-  } else {
+  int64_t* lhs_header = (int64_t*)lhs->storage;
+  int64_t* rhs_header = (int64_t*)rhs->storage;
+  if (lhs_header[1] != rhs_header[1]) {
     return 0;
   }
+
+  void* lhs_payload = lhs->storage + ARRAY_HEADER;
+  void* rhs_payload = rhs->storage + ARRAY_HEADER;
+  for (int64_t i = 0; i < lhs_header[1]; ++i) {
+    void* a = &lhs_payload[i * elem_type->size];
+    void* b = &rhs_payload[i * elem_type->size];
+    if (elem_type->equal(a, b) == 0) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 void mvs_print_i64(int64_t value) {
